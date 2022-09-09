4acbc51f1281f743b9ea7488133a387194c65ebe
Hector Gonzalez <gorozco.hector@gmail.com>
2022-09-09 16:33:51 -0400

    Add form that looksup for city weather

--- src/main.js
+++ src/main.js
@@ -1,4 +1,37 @@
 import model from './model';
+import './style.css';
 
 // model.getWeather().then((result) => console.log(result));
-model.getForecast().then((result) => console.log(result));
+// model.getForecast().then((result) => console.log(result));
+
+const view = (function () {
+  function renderWeather(name, dataObj) {
+    const title = document.querySelector('h1');
+    const content = document.querySelector('p');
+    title.innerText = name;
+    content.innerText = dataObj.day;
+  }
+  return { renderWeather };
+}()
+);
+
+const controller = (function () {
+  const form = document.querySelector('form');
+  const inputCity = document.querySelector('#input-city');
+  const buttonSubmit = document.querySelector('form input[type=submit]');
+
+  // After pressing submit, look weather and forecast of new city
+  async function lookupNewCity(e) {
+    if (form.checkValidity()) {
+      e.preventDefault();
+      const cityName = inputCity.value;
+      const weather = await model.getWeather(cityName).then((result) => result);
+      const forecast = await model.getForecast(cityName).then((result) => result);
+      //
+      view.renderWeather(cityName, weather);
+    }
+  }
+
+  buttonSubmit.addEventListener('click', lookupNewCity);
+}()
+);
