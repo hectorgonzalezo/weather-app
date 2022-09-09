cbac0efd9ca9a34d2fb01b80be72ef02f9f4a67a
Hector Gonzalez <gorozco.hector@gmail.com>
2022-09-09 16:03:42 -0400

    Get 5 day forecast

--- src/main.js
+++ src/main.js
@@ -1,1 +1,4 @@
 import model from './model';
+
+// model.getWeather().then((result) => console.log(result));
+model.getForecast().then((result) => console.log(result));
--- src/model.js
+++ src/model.js
@@ -1,8 +1,23 @@
 const model = (function model() {
-  async function callAPI(cityName = 'London') {
+  // Gets data from openWeather JSON and adds it to target empty object
+  function extractWeatherData(original) {
+    const target = {};
+    // Selection of values
+    target.day = new Date(original.dt * 1000);
+    target.temp = original.main.temp;
+    target.description = original.weather[0].description;
+    target.feels_like = original.main.feels_like;
+    target.humidity = original.main.humidity;
+    target.coord = original.coord;
+    target.wind_speed = original.wind.speed;
+    target.cloudiness = original.clouds.all;
+
+    return target;
+  }
+  async function callAPI(cityName = 'London', type = 'weather') {
     // call the openWeather api
     const request = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=e2802a8fb9f851e53d09fe4eb9b16d38`,
+      `https://api.openweathermap.org/data/2.5/${type}?q=${cityName}&appid=e2802a8fb9f851e53d09fe4eb9b16d38`,
       { mode: 'cors' },
     );
     // if the request was successfull, return the data
@@ -16,23 +31,28 @@ const model = (function model() {
 
   // Extracts data from getWeather() for every argument
   async function getWeather(cityName) {
-    const dataObject = {};
-    await callAPI(cityName)
-      .then((json) => {
-        // get all of these values and store them in dataObject
-        dataObject.temp = json.main.temp;
-        dataObject.description = json.weather[0].description;
-        dataObject.feels_like = json.main.feels_like;
-        dataObject.humidity = json.main.humidity;
-        dataObject.coord = json.coord;
-        dataObject.wind_speed = json.wind.speed;
-        dataObject.cloudiness = json.clouds.all;
-      });
+    // get all a selection of values and store them in dataObject
+    const dataObject = await callAPI(cityName, 'weather')
+      .then((data) => extractWeatherData(data));
 
     return dataObject;
   }
 
-  return { getWeather };
+  // Gets forecast for the next numDays days
+  async function getForecast(cityName, numDays = 5) {
+    const rawDataList = await callAPI(cityName, 'forecast')
+      .then((data) => {
+        // get the first five days
+        const fiveDays = data.list.slice(0, numDays);
+        return fiveDays;
+      });
+    // Extract selected data
+    const dataList = rawDataList.map((data) => extractWeatherData(data));
+
+    return dataList;
+  }
+
+  return { getWeather, getForecast };
 }());
 
 export default model;
